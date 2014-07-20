7cfef64932fe745dfc66900ad0eb470ba7b55506
EthanSargent <ethan.sargent.1250@gmail.com>
2014-07-20 16:36:35 -0700

    Edited Creases Factor

--- src/en/neuralnet/ocr/factors/Creases.java
+++ src/en/neuralnet/ocr/factors/Creases.java
@@ -1,8 +1,22 @@
 package en.neuralnet.ocr.factors;
 
 public class Creases implements Factor {
-
+	
+	public double tolerance;
 	public float calculate( float[][] img ) {
-		
+		//returns the number of creases
+			//if number has a curve, return 0
+		while (tolerance < 1) {
+			
+		}
+		return 10.6f;
 	}
+	public String getName(){
+		return "Creases";
+	}
+	public float getWeight() {
+		return 1;
+	}
+
 }
+
--- src/en/neuralnet/ocr/factors/Factor.java
+++ src/en/neuralnet/ocr/factors/Factor.java
@@ -7,6 +7,7 @@ public interface Factor {
 	 * @param img
 	 * @return
 	 */
+	
 	public float calculate(float[][] img);
 	
 	/**
