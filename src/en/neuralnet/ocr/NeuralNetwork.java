23d1628cefe2a8a574de1438a0e14ec278bff09d
Greg Carlin <gregthegeek@optonline.net>
2014-07-20 12:45:35 -0700

    Started main and factor interface

--- src/en/neuralnet/ocr/NeuralNetwork.java
+++ src/en/neuralnet/ocr/NeuralNetwork.java
@@ -1,7 +1,25 @@
 package en.neuralnet.ocr;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.HashMap;
+
+import javax.imageio.ImageIO;
+
+import en.neuralnet.ocr.factors.Factor;
+
 public class NeuralNetwork {
 	public static void main(String[] args) {
+		if(args.length < 1) throw new IllegalArgumentException("Must specify image path.");
 		
+		BufferedImage bi = ImageIO.read(new File(args[0]));
+		
+		
+		// maps potential characters to the probability that the mystery character matches that potential character
+		HashMap<Character,Float> probabilities = new HashMap<Character,Float>();
+		Factor[] factors = {}; // TODO fill with factors
+		for(Factor f : factors) {
+			float val = f.calculate(img);
+		}
 	}
 }
---
+++ src/en/neuralnet/ocr/factors/Factor.java
@@ -0,0 +1,11 @@
+package en.neuralnet.ocr.factors;
+
+public interface Factor {
+	/**
+	 * Calculates the value of this factor for the given image (represented by a 2D array of grayscale values 0-1)
+	 * 
+	 * @param img
+	 * @return
+	 */
+	public float calculate(float[][] img);
+}
