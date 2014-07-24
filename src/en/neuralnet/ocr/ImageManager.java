04f083ac693cb80ac149f9a907b234e77a7617a8
Greg Carlin <gregthegeek@optonline.net>
2014-07-24 14:57:15 -0700

    More training and more stats

--- src/en/neuralnet/ocr/ImageManager.java
+++ src/en/neuralnet/ocr/ImageManager.java
@@ -6,8 +6,9 @@ import java.io.IOException;
 
 public class ImageManager {
 	private static final String DATA_FOLDER = "C:\\Users\\scamper\\My Documents\\Neural network\\data\\";
-	private static final String IMAGE_FILE = "t10k-images-idx3-ubyte";
-	private static final String LABEL_FILE = "t10k-labels-idx1-ubyte";
+	private static final boolean IS_LARGE_DATA = false;
+	private static final String IMAGE_FILE = IS_LARGE_DATA ? "train-images-idx3-ubyte" : "t10k-images-idx3-ubyte";
+	private static final String LABEL_FILE = IS_LARGE_DATA ? "train-labels-idx1-ubyte" : "t10k-labels-idx1-ubyte";
 	
 	public static int[] getLabels() {
 		DataInputStream labelInputStream;
@@ -39,7 +40,7 @@ public class ImageManager {
 			int numCols = imgInputStream.readInt();
 			
 			double[][] images = new double[numImgs][numRows * numCols];
-			for(int i=0; i<images.length; i++) {
+			for( int i=0; i<images.length; i++) {
 				for(int j=0; j<images[i].length; j++) {
 					images[i][j] = imgInputStream.readUnsignedByte() / 255.0;
 				}
--- src/en/neuralnet/ocr/NeuralNetwork.java
+++ src/en/neuralnet/ocr/NeuralNetwork.java
@@ -1,97 +1,109 @@
 package en.neuralnet.ocr;
 
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
 public class NeuralNetwork {
 	private static final double ALPHA = 0.9;
 	private static final char[] CHARS = "0123456789".toCharArray();
-	private static final boolean TRAINING = false;
+	private static final int TRAINING_LOOPS = 0;
 	
+	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 		int[] labels = ImageManager.getLabels();
 		ImageData imageData = ImageManager.getImages();
 		double[][] images = imageData.getImages();
 		int imgSize = imageData.getNumCols() * imageData.getNumRows();
-		
+
 		if(labels.length != images.length) throw new IllegalArgumentException("Number of labels is not equal to number of images.");
-		
+
 		System.out.println("retrieved image data");
-		
+
 		WeightManager weightManager = new WeightManager(imgSize, CHARS);
-		
+
 		Neuron[] neurons = new Neuron[CHARS.length];
 		for(int j=0; j<neurons.length; j++) {
 			neurons[j] = new Neuron();
 			neurons[j].setWeights(weightManager.getWeights(CHARS[j]));
 			neurons[j].setBias(weightManager.getBias(CHARS[j]));
 		}
-		
+
 		System.out.println("initialized neurons");
-		
-		int correct = 0;
-		
-		for(int i=0; i<images.length; i++) {
-			// Propagate the inputs forward to compute the outputs
-			double[] weightedSums = new double[neurons.length];
-			double[] outputs = new double[neurons.length];
-			for(int j=0; j<weightedSums.length; j++) {
-				weightedSums[j] = neurons[j].getOutput(images[i]);
-				outputs[j] = sigmoidFunction(weightedSums[j]);
-			}
-			
-			if(TRAINING) {
-			
-				double[] deltas = new double[neurons.length];
-				for(int j=0; j<deltas.length; j++) {
-					deltas[j] = sigmoidPrime(weightedSums[j]) * ((labels[i] == j ? 1.0 : 0.0) - outputs[j]);
+
+		int[] results = new int[CHARS.length];
+		int loops = TRAINING_LOOPS > 0 ? TRAINING_LOOPS : 1;
+
+		for(int a=0; a<loops; a++) {
+			System.out.println("a = " + a);
+			for(int i=0; i<images.length; i++) {
+				// Propagate the inputs forward to compute the outputs
+				double[] weightedSums = new double[neurons.length];
+				double[] outputs = new double[neurons.length];
+				for(int j=0; j<weightedSums.length; j++) {
+					weightedSums[j] = neurons[j].getOutput(images[i]);
+					outputs[j] = sigmoidFunction(weightedSums[j]);
 				}
-				
-				for(int j=0; j<neurons.length; j++) {
-					double[] weights = neurons[j].getWeights();
-					double bias = neurons[j].getBias();
-					for(int k=0; k<weights.length; k++) {
-						weights[k] += ALPHA * images[i][k] * deltas[j];
-						bias += ALPHA * images[i][k] * deltas[j];
-						//System.out.printf("neuron %d weight %d set to %f%n", j, k, weights[k]);
+
+				if(TRAINING_LOOPS > 0) {
+
+					double[] deltas = new double[neurons.length];
+					for(int j=0; j<deltas.length; j++) {
+						deltas[j] = sigmoidPrime(weightedSums[j]) * ((labels[i] == j ? 1.0 : 0.0) - outputs[j]);
 					}
-					neurons[j].setWeights(weights);
-					neurons[j].setBias(bias);
-					char c = Character.forDigit(j, 10);
-					weightManager.setWeights(c, weights);
-					weightManager.setBias(c, bias);
-				}
-			
-			} else {
-				
-				double max = -Double.MAX_VALUE;
-				int maxIndex = -1;
-				for(int j=0; j<outputs.length; j++) {
-					if(outputs[j] > max) {
-						max = outputs[j];
-						maxIndex = j;
+
+					for(int j=0; j<neurons.length; j++) {
+						double[] weights = neurons[j].getWeights();
+						double bias = neurons[j].getBias();
+						for(int k=0; k<weights.length; k++) {
+							weights[k] += ALPHA * images[i][k] * deltas[j];
+							bias += ALPHA * images[i][k] * deltas[j];
+							//System.out.printf("neuron %d weight %d set to %f%n", j, k, weights[k]);
+						}
+						neurons[j].setWeights(weights);
+						neurons[j].setBias(bias);
+						char c = Character.forDigit(j, 10);
+						weightManager.setWeights(c, weights);
+						weightManager.setBias(c, bias);
 					}
+
+				} else {
+
+					SortedMap<Double, Integer> solutions = new TreeMap<Double, Integer>();
+					for (int j = 0; j < CHARS.length; j ++) {
+						solutions.put(outputs[j],j);
+					}
+					System.out.println("ANSWER: " + labels[i]);
+					Set<Entry<Double, Integer>> solSet = solutions.entrySet();
+					int j = 0;
+					for (Entry<Double, Integer> e : solSet) {
+						if(j >= 5) System.out.println("\tGuess " + (results.length - j) + ": " + e.getValue());
+						if(e.getValue() == labels[i]) results[results.length - j - 1]++;
+						j++;
+					}
+					
+					System.out.println();
 				}
-				assert maxIndex >= 0;
-				if(maxIndex == labels[i]) correct++;
-				
-				System.out.println(" GUESS: " + maxIndex);
-				System.out.println("ANSWER: " + labels[i]);
-				System.out.println();
+
 			}
 		}
-		
-		if(TRAINING) {
+
+		if(TRAINING_LOOPS > 0) {
 			System.out.println("saving weights");
 			weightManager.save();
 		} else {
-			System.out.printf("%d/%d correct.%n", correct, images.length);
-			System.out.printf("That's %f%%%n", ((double) correct) / images.length * 100);
+			for (int i = 0; i < 10; i ++ ) {
+				System.out.printf("%f%% guessed as %d%n", ((double) results[i]) / images.length * 100, i + 1);
+			}
 		}
 	}
-	
+
 	private static double sigmoidPrime(double x) {
 		double temp = sigmoidFunction(x);
 		return temp * (1 - temp);
 	}
-	
+
 	public static double sigmoidFunction(double in) {
 		return (1/(1 + Math.exp(-in)));
 	}
